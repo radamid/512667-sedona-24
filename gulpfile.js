41340b06c0c599ddb7c5f326a7ff9f24ebb3b3cf
radamid <dimasjw@bk.ru>
2022-01-03 16:39:34 +0500

    Перемещение текста интро в спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -70,13 +70,13 @@ const createWebp = () => {
 // SVG
 
 const svg = () => {
-  return gulp.src(['source/img/**/*.svg', '!source/img/logo-*.svg', '!source/img/button-*.svg'])
+  return gulp.src(['source/img/**/*.svg', '!source/img/logo-*.svg', '!source/img/button-*.svg', '!source/img/intro-*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest("build/img"));
 }
 
 const sprite = () => {
-  return gulp.src(['source/img/logo-*.svg', 'source/img/button-*.svg'])
+  return gulp.src(['source/img/logo-*.svg', 'source/img/button-*.svg', 'source/img/intro-*.svg'])
     .pipe(svgo())
     .pipe(svgstore({
       inlineSvg: true
