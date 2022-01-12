18bb3564f03ae43fb2a339c8abdeb83e1da8014c
radamid <dimasjw@bk.ru>
2022-01-12 20:15:20 +0500

    Исправления по рекомендациям наставника

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,66 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import less from 'gulp-less';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import csso from 'postcss-csso';
-import rename from 'gulp-rename';
-import htmlmin from 'gulp-htmlmin';
-import terser from 'gulp-terser';
-import squoosh from 'gulp-libsquoosh';
-import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
-import del from 'del';
-import browser from 'browser-sync';
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import less from "gulp-less";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import htmlmin from "gulp-htmlmin";
+import terser from "gulp-terser";
+import squoosh from "gulp-libsquoosh";
+import svgo from "gulp-svgmin";
+import svgstore from "gulp-svgstore";
+import del from "del";
+import browser from "browser-sync";
 
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/less/style.less', { sourcemaps: true })
+  return gulp.src("source/less/style.less", { sourcemaps: true })
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer(),
       csso()
     ]))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css", { sourcemaps: "." }))
     .pipe(browser.stream());
 }
 
 // HTML
 
 const html = () => {
-  return gulp.src('source/*.html')
+  return gulp.src("source/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest("build"));
 }
 
 // Scripts
 
 const scripts = () => {
-  return gulp.src('source/js/**/*.js')
+  return gulp.src("source/js/**/*.js")
     .pipe(terser())
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest("build/js"));
 }
 
 // Images
 
 const optimizeImages = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
+  return gulp.src("source/img/**/*.{jpg,png}")
     .pipe(squoosh())
     .pipe(gulp.dest("build/img"));
 }
 
 const copyImages = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
+  return gulp.src("source/img/**/*.{jpg,png}")
     .pipe(gulp.dest("build/img"));
 }
 
 // WebP
 
 const createWebp = () => {
-  return gulp.src('source/img/**/*.{jpg, png}')
+  return gulp.src("source/img/**/*.{jpg, png}")
     .pipe(squoosh({
       webp: {}
     }))
@@ -70,18 +70,18 @@ const createWebp = () => {
 // SVG
 
 const svg = () => {
-  return gulp.src(['source/img/**/*.svg', '!source/img/logo-*.svg', '!source/img/button-*.svg', '!source/img/intro-*.svg'])
+  return gulp.src(["source/img/**/*.svg", "!source/img/logo-*.svg", "!source/img/button-*.svg", "!source/img/intro-*.svg"])
     .pipe(svgo())
     .pipe(gulp.dest("build/img"));
 }
 
 const sprite = () => {
-  return gulp.src(['source/img/logo-*.svg', 'source/img/button-*.svg', 'source/img/intro-*.svg'])
+  return gulp.src(["source/img/logo-*.svg", "source/img/button-*.svg", "source/img/intro-*.svg"])
     .pipe(svgo())
     .pipe(svgstore({
       inlineSvg: true
     }))
-    .pipe(rename('sprite.svg'))
+    .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 }
 
@@ -89,20 +89,20 @@ const sprite = () => {
 
 const copy = (done) => {
   gulp.src([
-    'source/fonts/*.{woff2,woff}',
-    'source/*.ico',
-    'source/*.webmanifest'
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/*.webmanifest"
   ], {
-    base: 'source'
+    base: "source"
   })
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest("build"))
   done();
 }
 
 // Clean
 
 const clean = () => {
-  return del('build');
+  return del("build");
 }
 
 // Server
@@ -110,7 +110,7 @@ const clean = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -129,9 +129,9 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/js/**/*.js', gulp.series(scripts));
-  gulp.watch('source/*.html', gulp.series(html, reload));
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/**/*.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 // Build
